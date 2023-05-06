c4507e438c3a6d3eb223463cc585249efa7f6e5b
Niharika-1BM19CS100 <71308403+Niharika-1BM19CS100@users.noreply.github.com>
2023-05-06 16:42:28 +0530

    Update train.py

--- train.py
+++ train.py
@@ -88,32 +88,45 @@ def main():
         train_csv_path=config.DATASET + "/train.csv", test_csv_path=config.DATASET + "/test.csv"
     )
 
-    if config.LOAD_MODEL:
-        load_checkpoint(
-            config.CHECKPOINT_FILE, model, optimizer, config.LEARNING_RATE
-        )
+    # if config.LOAD_MODEL:
+    #     load_checkpoint(
+    #         config.CHECKPOINT_FILE, model, optimizer, config.LEARNING_RATE
+    #     )
 
     scaled_anchors = (
         torch.tensor(config.ANCHORS)
         * torch.tensor(config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
     ).to(config.DEVICE)
+    print(config.NUM_EPOCHS)
+    for epoch in range(config.NUM_EPOCHS):
+        #plot_couple_examples(model, test_loader, 0.6, 0.5, scaled_anchors)
+        train_fn(train_loader, model, optimizer, loss_fn, scaler, scaled_anchors)
 
-    check_class_accuracy(model, test_loader, threshold=config.CONF_THRESHOLD)
-    pred_boxes, true_boxes = get_evaluation_bboxes(
-        test_loader,
-        model,
-        iou_threshold=config.NMS_IOU_THRESH,
-        anchors=config.ANCHORS,
-        threshold=config.CONF_THRESHOLD,
-    )
-    mapval = mean_average_precision(
-        pred_boxes,
-        true_boxes,
-        iou_threshold=config.MAP_IOU_THRESH,
-        box_format="midpoint",
-        num_classes=config.NUM_CLASSES,
-    )
-    print(f"MAP: {mapval.item()}")
+        if config.SAVE_MODEL:
+           save_checkpoint(model, optimizer, filename=f"checkpoint.pth.tar")
+
+        print(f"Currently epoch {epoch}")
+        print("On Train Eval loader:")
+        print("On Train loader:")
+        check_class_accuracy(model, train_loader, threshold=config.CONF_THRESHOLD)
+
+        # if epoch > 0 and epoch % 3 == 0:
+        #     check_class_accuracy(model, test_loader, threshold=config.CONF_THRESHOLD)
+        #     pred_boxes, true_boxes = get_evaluation_bboxes(
+        #         test_loader,
+        #         model,
+        #         iou_threshold=config.NMS_IOU_THRESH,
+        #         anchors=config.ANCHORS,
+        #         threshold=config.CONF_THRESHOLD,
+        #     )
+            # mapval = mean_average_precision(
+            #     pred_boxes,
+            #     true_boxes,
+            #     iou_threshold=config.MAP_IOU_THRESH,
+            #     box_format="midpoint",
+            #     num_classes=config.NUM_CLASSES,
+            # )
+            # print(f"MAP: {mapval.item()}")
 
 
 if __name__ == "__main__":
